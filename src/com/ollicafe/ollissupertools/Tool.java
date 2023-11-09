008b224df04ec65d0e5bbc505f3ea1187620f79f
Olli's Cafe <olli@ollicafe.com>
2023-11-08 20:51:21 -0800

    add axe

--- src/com/ollicafe/ollissupertools/Tool.java
+++ src/com/ollicafe/ollissupertools/Tool.java
@@ -3,6 +3,7 @@ package com.ollicafe.ollissupertools;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Tool {
 	
@@ -13,6 +14,9 @@ public class Tool {
 	
 	private ShapedRecipe pickaxe() {
 		ItemStack pick = new ItemStack(Material.NETHERITE_PICKAXE);
+		ItemMeta meta = pick.getItemMeta();
+		meta.setDisplayName("Super Pickaxe");
+		pick.setItemMeta(meta);
 		
 		
 		ShapedRecipe superPick = new ShapedRecipe(pick);
@@ -24,6 +28,24 @@ public class Tool {
 		superPick.setIngredient('a', Material.ANVIL);
 		
 		return superPick;
+	}
+	
+	private ShapedRecipe axe() {
+		ItemStack axe = new ItemStack(Material.NETHERITE_AXE);
+		ItemMeta meta = axe.getItemMeta();
+		meta.setDisplayName("Super Axe");
+		axe.setItemMeta(meta);
+		
+		ShapedRecipe superAxe = new ShapedRecipe(axe);
+		
+		superAxe.shape("ttt"," s "," a ");
+
+		superAxe.setIngredient('t', Material.NETHERITE_AXE);
+		superAxe.setIngredient('s', Material.NETHER_STAR);
+		superAxe.setIngredient('a', Material.ANVIL);
+		
+		return superAxe;
+		
 	}
 
 }
